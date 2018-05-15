b94ad8c73be03c2875f31eae5762d1a31dfdddcf
nathan-edwards <39305639+nathan-edwards@users.noreply.github.com>
2018-05-15 11:26:20 -0400

    Update qcmetrics.sh

--- metrics/qcmetrics.sh
+++ metrics/qcmetrics.sh
@@ -1,1 +1,18 @@
+#!/bin/sh
 
+if [ $# -lt 3 ]; then
+  echo "Usage: qcmetrics.sh <mzMLfile> <mzIdentMLfile> <qcmetricsfile>" 1>&2
+  exit 1;
+fi
+
+BASE=`readlink -f "$0"`
+BASE=`dirname "$BASE"`
+
+METRICR="$BASE/qcmetrics.r"
+
+# For Edwards lab installation of R and java
+export PATH="/tools/bin:$PATH"
+
+RSCRIPT=${RSCRIPT:-Rscript} 
+
+"$RSCRIPT" "$METRICR" "$1" "$2" "$3" >/dev/null 2>&1 || exit 1
